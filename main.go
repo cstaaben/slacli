d2a855233639e742ed56de4254c4b40c49cbcacd
Corbin Staaben <cstaaben@gmail.com>
2024-03-19 23:26:45 -0700

    chore(license): use GPL 3.0

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
+/*
+	slacli - a TUI for Slack
+	Copyright (C) 2024 Corbin Staaben
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
 
-	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/bubbletea-app-template/internal/model"
 )
 
+/*
 type errMsg error
 
 type model struct {
@@ -65,11 +81,11 @@ func (m model) View() string {
 	}
 	return str
 }
+*/
 
 func main() {
-	p := tea.NewProgram(initialModel())
+	p := tea.NewProgram(model.New())
 	if _, err := p.Run(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatalln(err)
 	}
 }
